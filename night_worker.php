c758465a864320f58af8b49cded2d5748543f041
kirya <cloudkserg11@gmail.com>
2018-02-09 17:03:38 +0300

    fixed

--- night_worker.php
+++ night_worker.php
@@ -1,5 +1,15 @@
 <?php
-$rootDir = './';
+$rootDir = '.';
+
+spl_autoload_register(function ($class) use ($rootDir) {
+    $file = str_replace('\\', DIRECTORY_SEPARATOR, $class).'.php';
+    $file = $rootDir . '/' . str_replace('Src', 'src', $file);
+    if (file_exists($file)) {
+        require $file;
+        return true;
+    }
+    return false;
+});
 
 //load config
 $config = new \Src\Core\Config\Config($rootDir . '/config/config.php');
